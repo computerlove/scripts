203a89a10a5cf273ceddae925e7b5b37766f90fc
Marvin B. Lillehaug <marvin.lillehaug@gmail.com>
2013-03-30 19:23:37 +0100

    Fixx

--- timelaps/cam.sh
+++ timelaps/cam.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
-ROOT="/media/windows/Webcam/"
+ROOT="/media/windows/Webcam"
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 DAY=$(date +"%d")
 TIME=$(date +"%T")
-LATESTFILE=$ROOT"latest.jpeg"
+LATESTFILE=$ROOT"/latest.jpeg"
 SOURCEDIR="/home/hildegunn"
+LOGFILE=${SOURCEDIR}"/webcam.log.gz"
 
-function mkdirIfNonExisting {
-  if [ ! -d "$1" ]; then
-    echo "Creating $FILE"
-    mkdir $FILE
-  fi
+function log {
+  echo "$(date)  ${1}" | gzip >> ${LOGFILE}
 }
-DESTINATIONFILE=$ROOT"/"$YEAR
-mkdirIfNonExisting $DESTINATIONFILE
-DESTINATIONFILE=$DESTINATIONFILE"/"$MONTH
-mkdirIfNonExisting $DESTINATIONFILE
-DESTINATIONFILE=$DESTINATIONFILE"/"$DAY
-mkdirIfNonExisting $DESTINATIONFILE
 
-for IMAGE in ${SOURCEDIR}/Image*.jpg
+DESTINATIONFILE=$ROOT"/"$YEAR"/"$MONTH"/"$DAY
+if [ ! -d "$DESTINATIONFILE" ]; then
+    log "Creating $DESTINATIONFILE"
+    mkdir -p $DESTINATIONFILE 
+fi
+
+IMAGES=$(ls ${SOURCEDIR}/Image*) 
+for IMAGE in ${IMAGES}
 do 
+   log "Found ${IMAGE}"
    FILE=`stat -c %y ${IMAGE} | sed -r 's#([0-9]{4})-([0-9]{1,2})-([0-9]{1,2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})(.*)#\4#g'`
    FILE=$DESTINATIONFILE"/"$FILE".jpeg"
+   log "Saving to ${FILE}"
    STAMP=`stat -c %y ${IMAGE} | sed -r 's#([0-9]{4})-([0-9]{1,2})-([0-9]{1,2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})(.*)#\4 \3.\2.\1#g'`
-   echo "Stamping $IMAGE and saving to $FILE"
+   log "Stamping $IMAGE and saving to $FILE"
    convert -fill white -box '#00000080' -gravity South -pointsize 40 -annotate +0+5 "${STAMP}" ${IMAGE} ${FILE}
    rm ${IMAGE}
 done
@@ -34,5 +35,7 @@ if [ -f "$FILE" ]; then
   rm $LATESTFILE
   ln -s $FILE $LATESTFILE
   scp -P 27897 $FILE root@computerlove.dyndns.org:/var/www/holkestadcam.jpeg
+else 
+  log "No image found"
 fi
 
--- timelaps/timelapsplx.sh
+++ timelaps/timelapsplx.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
-DIR=$1
-VIDEO=${DIR}/video.mp4
+DIR="/media/windows/Webcam"
+TEMPDIR="/tmp/timelapstemp"
+FROM=$1
+TO=$2
+VIDEO="${DIR}/${FROM}-${TO}.mp4"
+
+if [ "$1" == "usage" ]
+then
+   echo "timelapsplx.sh yyyy-mm-dd yyyy-mm-dd"
+   exit 0
+fi
+
 if [ -f "${VIDEO}" ]
 then
    echo "${VIDEO} exists"
 else
-   STAMPDIR=${DIR}/timelaps
-   mkdir $TEMPDIR
+   mkdir -p $TEMPDIR
+
+   IMAGES=$(find ${DIR} -name "*.jpeg" -newermt ${FROM} ! -newermt ${TO} | sort)
+   echo "find ${DIR} -name "*.jpeg" -newermt ${FROM} ! -newermt ${TO} | sort"
+
    I=0
-   for image in ${DIR}/*
+   for image in ${IMAGES}
    do
-      if [ ! -f ${image} ]
-      then
-         STAMP=`stat -c %y $image | sed -r 's#([0-9]{4})-([0-9]{1,2})-([0-9]{1,2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})(.*)#\3.\2.\1 \4#g'`
-         convert -fill white -box '#00000080' -gravity South -pointsize 40 -annotate +0+5 "${STAMP}" $image $STAMPDIR/img$(printf %03d $I).jpeg
-         I=$(($I+1))
-      fi
+     ln -s ${image} "${TEMPDIR}/img$(printf %05d $I).jpeg"
+     I=$(($I+1))
    done
-   cd $STAMPDIR
-   ffmpeg -f image2 -r 15 -i img%03d.jpeg -vcodec libx264 -vpre hq -crf 25 ${VIDEO} 
+   cd $TEMPDIR
+   ffmpeg -f image2 -r 15 -s svga -i img%05d.jpeg -vcodec libx264 -vpre hq -crf 25 ${VIDEO} 
+   rm -rf $TEMPDIR
 fi
